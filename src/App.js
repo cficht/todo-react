1cdd461db7c120b4d2b9dbd930b18594fe3d8c1d
cficht <chris.ficht@gmail.com>
2020-02-25 17:15:08 -0800

    initialized react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { 
+  Route, 
+  Switch,
+  Link,
+  BrowserRouter as Router, 
+} from 'react-router-dom';
+import Add from './Add.js';
+import List from './List.js';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        APP
+        
+      </div>
+    )
+  }
+}
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Item from './Item.js';
+
+export default class List extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
